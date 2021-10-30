2b113e766cef82da91657d1009d1a6d7e9d64ab8
ManishBisht <manishbisht9711@gmail.com>
2021-10-30 22:00:35 +0530

    viewers component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import Imgslider from "./Imgslider";
+import Viewer from "./Viewer";
 function Home() {
   return (
     <Container>
       <Imgslider />
+      <Viewer />
     </Container>
   );
 }
